5fafd45ac9d1e22e16863b52f3caaf9da4cc1379
Zikani singini <46520540+zikani@users.noreply.github.com>
2025-03-23 12:47:33 +0200

    Update bullet.py
    fix some left some lol

--- bullet.py
+++ bullet.py
@@ -19,3 +19,30 @@ class Bullet:
     def is_off_screen(self):
         """Check if bullet is outside map boundaries"""
         return not (0 <= self.rect.x <= MAP_SIZE and 0 <= self.rect.y <= MAP_SIZE)
+        
+    def draw(self, screen, camera_x, camera_y):
+        """Draw bullet with trail effect"""
+        # Draw bullet body
+        pygame.draw.rect(
+            screen,
+            self.color,
+            (
+                int(self.rect.x - camera_x),
+                int(self.rect.y - camera_y),
+                self.rect.width,
+                self.rect.height
+            )
+        )
+        
+        # Add bullet trail
+        trail_length = 8
+        trail_x = int(self.rect.centerx - self.vx * trail_length - camera_x)
+        trail_y = int(self.rect.centery - self.vy * trail_length - camera_y)
+        
+        pygame.draw.line(
+            screen,
+            (self.color[0]//2, self.color[1]//2, self.color[2]//2),
+            (int(self.rect.centerx - camera_x), int(self.rect.centery - camera_y)),
+            (trail_x, trail_y),
+            max(1, self.rect.width // 2)
+        )
